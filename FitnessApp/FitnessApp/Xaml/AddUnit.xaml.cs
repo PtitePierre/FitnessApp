8759c3a8bc930c14568188fbbc1ba7f84aa7a9b3
PtitePierre <ptite_pierre71@hotmail.fr>
2018-04-12 16:55:11 +0100

    Edit navigation between edit page and listview

---
+++ FitnessApp/FitnessApp/Xaml/AddUnit.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using FitnessApp.Portable;
+
+namespace FitnessApp
+{
+	//[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AddUnit : ContentPage
+	{
+        private ContentPage Page;
+		public AddUnit (ContentPage page, Unit unit = null)
+		{
+            Page = page;
+			InitializeComponent ();
+            
+            if (unit != null)
+            {
+                in_Code.Text = unit.Code;
+                in_Name.Text = unit.Name;
+            }
+            else
+            {
+                DependencyService.Get<IMessage>().shorttime("no unit parameter");
+            }
+		}
+
+        private void Button_CancelClicked(object sender, EventArgs e)
+        {
+            Application.Current.MainPage = new NavigationPage(Page);            
+        }
+	}
+}
--- FitnessApp/FitnessApp/Xaml/ViewListJson.xaml.cs
+++ FitnessApp/FitnessApp/Xaml/ViewListJson.xaml.cs
@@ -23,10 +23,28 @@ namespace FitnessApp
 
 
             InitializeComponent();
+            
 
             // TO DO : Binding Data units to ListView
             UnitList.ItemsSource = units;
             SportList.ItemsSource = sports;
+        }
+
+        private void AddSportClicked(object sender, EventArgs e)
+        {
+            //DisplayAlert("Ploup", "Ploup'z alert", "Quit!");
+        }
+
+        private void AddUnitClicked(object sender, EventArgs e)
+        {
+            if(UnitList.SelectedItem != null)
+            {
+                Application.Current.MainPage = new NavigationPage(new AddUnit(this, (Unit)UnitList.SelectedItem));
+            }
+            else
+            {
+                Application.Current.MainPage = new NavigationPage(new AddUnit(this));
+            }
         }
     }
 }
